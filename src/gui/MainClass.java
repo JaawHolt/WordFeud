9fd280cf59e696aa58f5f2e360e771480b8b8836
Danny van der Biezen <Dannyvdbiezen@outlook.com>
2013-04-18 00:15:43 +0200

    Main gemaakt
    om github bij iedereen te testen wil ik graag dat je dit bestand kan
    openen in eclipse, je naam kan toevoegen en dit weer terug kan uploaden
    naar github.

---
+++ src/gui/MainClass.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class MainClass {
+
+	/**
+	 * @authors Groep Min04SOc
+	 */
+	public static void main(String[] args) {
+		//zet hier je naam neer en upload de nieuwe versie naar github
+		System.out.println("Danny");
+	}
+
+}
